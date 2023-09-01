7261d014779d3e840da147747a9a627668dab0c8
Folake Salako <138071820+FlakysB@users.noreply.github.com>
2023-09-01 07:14:11 +0100

    Create sales_across_country.sql

---
+++ sales_across_country.sql
@@ -0,0 +1,14 @@
+SELECT 
+    country,
+    COUNT(p.productID) AS product_sold,
+    SUM (od.quantity) AS total_sales,
+    ROUND(SUM(od.unitPrice * od.quantity * (1 - od.discount)), 2) AS total_sales_amount,
+FROM
+    `project-3-394210.Northwind_Traders.customers` c
+LEFT JOIN 
+    `Northwind_Traders.orders` o ON c.customerID = o.customerID
+LEFT JOIN 
+    `Northwind_Traders.order_details` od ON o.orderID = od.orderID
+LEFT JOIN `Northwind_Traders.products` p ON od.productID = p.productID
+GROUP BY 
+    1   ;
